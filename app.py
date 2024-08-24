23c7d58480c7b48a3302a4720ecc240000edfa20
Yu <102244563+12yuuuu@users.noreply.github.com>
2024-08-24 21:54:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ from linebot.models import (
     PostbackEvent, MemberJoinedEvent
 )
 import os
-from message import imagemap_message, buttons_message, Confirm_Template, Carousel_Template
-from Function import function_list
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
@@ -26,53 +23,24 @@ QA_DICT = {
     # Add more Q&A pairs here...
 }
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
     if msg in QA_DICT:
         response = QA_DICT[msg]
-    elif '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-        return
     else:
-        response = "對不起，我不理解您的問題。請嘗試其他問題或查看功能列表。"
+        response = "對不起，我不理解您的問題。請嘗試其他問題。"
     
     message = TextSendMessage(text=response)
     line_bot_api.reply_message(event.reply_token, message)
