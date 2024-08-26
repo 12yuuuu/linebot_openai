478574dde14a5ceb35be314181be211f30af7802
Yu <102244563+12yuuuu@users.noreply.github.com>
2024-08-26 15:45:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-rom flask import Flask, request, abort
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
